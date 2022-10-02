477be58dbc6a6582e9fe79a0d51d2acaaf844f3c
Jae72 <jacarslanian@gmail.com>
2022-10-02 17:09:35 -0400

    commiting my settings page components

---
+++ jamfynder/src/components/SettingsPage/Settings.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Settings.css"
+const Hero = () => {
+    return(
+        <div className="Settings">
+            <h1>Manage Account</h1>
+            <h1>Sign Out</h1>
+            <h1>Delete Account</h1>
+        </div>
+    )
+}
+
+export default Settings;
