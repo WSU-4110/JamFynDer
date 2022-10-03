10b200fd2b82147c950d8ca20e1e552e69c3d65a
Chris Wilson <awil17@outlook.com>
2022-10-03 05:00:34 -0400

    Minor Edits

--- jamfynder/src/components/NavigationBar/NavigationBar.js
+++ jamfynder/src/components/NavigationBar/NavigationBar.js
@@ -1,38 +1,35 @@
-import React from 'react'
-import {
-  Link
-} from 'react-router-dom'
-
-import './NavigationBar.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./NavigationBar.css";
 
 const NavigationBar = () => {
-  return(
+  return (
     <header className="NavigationBar">
-        <nav>
-          <ul>
-            <Link to="/welcome">
-              <li>Home</li>
-            </Link>
-            
-            <Link to="/login">
-              <li>Login</li>
-            </Link>
+      <nav>
+        <ul>
+          <Link to="/welcome">
+            <li>Home</li>
+          </Link>
 
-            <Link to="/about_us">
-              <li>About Us</li>
-            </Link>
+          <Link to="/login">
+            <li>Login</li>
+          </Link>
 
-            <Link to="/settings">
-              <li>Settings</li>
-            </Link>
+          <Link to="/about_us">
+            <li>About Us</li>
+          </Link>
 
-            <Link to="/tutorial">
-              <li>Tutorial</li>
-            </Link>
-          </ul>
-        </nav>
-      </header>
-  )
-}
+          <Link to="/settings">
+            <li>Settings</li>
+          </Link>
+
+          <Link to="/tutorial">
+            <li>Tutorial</li>
+          </Link>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default NavigationBar;
