5ca7f34ea998a035bdbfb849886a8bf71cbaf457
Urban-Programmer <awil17@outlook.com>
2022-10-30 02:40:25 -0400

    Package the login as a exportable function

--- jamfynder/src/App.js
+++ jamfynder/src/App.js
@@ -9,17 +9,8 @@ import SettingsPage from "./components/SettingsPage";
 import AboutUs from "./components/AboutUs";
 import Player from "./components/Player";
 
-const CLIENT_ID = "f12088ba0b0c45018df4dad44b51b83d";
-const ENDPOINT = "https://accounts.spotify.com/authorize";
-const REDIRECT_URL = "http://localhost:3000/Player";
-const SPACE_DELIMITER = "%20";
-const SCOPES = ["user-read-currently-playing", "user-read-playback-state"];
-const SCOPES_URL_PARM = SCOPES.join(SPACE_DELIMITER);
-
 const App = () => {
-  const handleLogin = () => {
-    window.location = `${ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL}&scope=${SCOPES_URL_PARM}&response_type=token&show_dialog=true`;
-  };
+
   return (
     <Router>
       <div className="App">
