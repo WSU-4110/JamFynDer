7e1a94636e5fac7dcdfbaa286980087462d2848c
Ali Janbeih <alijanbeih7898@gmail.com>
2022-10-31 19:16:40 -0400

    Fixed bugs in my branch

--- jamfynder/src/components/AboutUs/index.js
+++ jamfynder/src/components/AboutUs/index.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-import AboutUs from './AboutUs'
-
-export default AboutUs
-=======
 import AboutUs from "./AboutUs";
 
 export default AboutUs;
->>>>>>> 4b7210f169f32f4c2a4b380882feffb4c40c21dc
