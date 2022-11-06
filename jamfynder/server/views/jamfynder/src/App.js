77857199aec8abad7d16d999fdf71363b5d561fb
Urban-Programmer <awil17@outlook.com>
2022-11-06 03:31:15 -0500

    Removed Switch for site navigation. Needs to be updated with Express routes

--- jamfynder/server/views/jamfynder/src/App.js
+++ jamfynder/server/views/jamfynder/src/App.js
@@ -1,25 +1,57 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import "./App.css";
+import WelcomePage from "./components/WelcomePage";
+import NavigationBar from "./components/NavigationBar";
+import TutorialPage from "./components/TutorialPage";
+import SettingsPage from "./components/SettingsPage";
+import AboutUs from "./components/AboutUs";
+import Player from "./components/Player";
+
+const CLIENT_ID = "f12088ba0b0c45018df4dad44b51b83d";
+const ENDPOINT = "https://accounts.spotify.com/authorize";
+const REDIRECT_URL = "http://localhost:3000/Player";
+const SPACE_DELIMITER = "%20";
+const SCOPES = ["user-read-currently-playing", "user-read-playback-state"];
+const SCOPES_URL_PARM = SCOPES.join(SPACE_DELIMITER);
+
+const App = () => {
+  const handleLogin = () => {
+    window.location = `${ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL}&scope=${SCOPES_URL_PARM}&response_type=token&show_dialog=true`;
+  };
+  return <body> Hello </body>;
+};
+//   return (
+//     // <Router>
+//     //   <div className="App">
+//     //     <NavigationBar />
+//     //     <div className="content">
+//     //       <Switch>
+//     //         <Route path="/welcome">
+//     //           <WelcomePage />
+//     //         </Route>
+
+//     //         <Route path="/Player">
+//     //           <Player />
+//     //         </Route>
+
+//     //         <Route path="/SettingsPage">
+//     //           <SettingsPage />
+//     //         </Route>
+
+//     //         <Route path="/about_us">
+//     //           <AboutUs />
+//     //         </Route>
+
+//     //         <Route path="/tutorial">
+//     //           <TutorialPage />
+//     //         </Route>
+//     //       </Switch>
+//     //     </div>
+//     //   </div>
+//     // </Router>
+//   // );
+// };
 
 export default App;
---
+++ jamfynder/server/views/jamfynder/src/components/TutorialPage/TutorialPage.js
@@ -0,0 +1,134 @@
+import React, { useState, useEffect } from "react";
+import "./TutorialPage.css"
+import "./Dropdown.js"
+import Listbox from "./Listbox";
+import Dropdown from "./Dropdown.js";
+import axios from 'axios'
+import Detail from "./Detail";
+
+
+
+
+const TutorialPage = () => {
+     
+
+    const clientID = "8bc35a75fa824f0b9ff3d0683c05fa82"
+    const secret = "ba291791b4854e7ba6fdd7a3abbb0814"
+
+    const data = [ 
+        {value: 1, name: 'A'},
+        {value: 2, name: 'B'},
+        {value: 3, name: 'C'},
+      ]; 
+ 
+      const [token, setToken] = useState('');
+      const [genres, setGenres] = useState({selectedGenre: '', listOfGenresFromAPI: []});
+      const [playlist, setPlaylist] = useState({selectedPlaylist: '', listOfPlaylistFromAPI: []});
+      const [tracks, setTracks] = useState({selectedTrack: '', listOfTracksFromAPI: []});
+      const [trackDetail, setTrackDetail] = useState(null);
+
+      useEffect(() => {
+
+        axios('https://accounts.spotify.com/api/token', {
+            headers: {
+                'Content-Type' : 'application/x-www-form-urlencoded',
+                'Authorization' : 'Basic ' + btoa(clientID + ':' + secret)      
+            },
+            data: 'grant_type=client_credentials',
+            method: 'POST'
+            })
+            .then(tokenResponse => {       
+            setToken(tokenResponse.data.access_token);
+            
+
+            axios('https://api.spotify.com/v1/browse/categories?country=US', {
+                method: 'GET',
+                headers: { 'Authorization' : 'Bearer ' + tokenResponse.data.access_token}
+              })
+              .then (genreResponse => {        
+                setGenres({
+                  selectedGenre: genres.selectedGenre, 
+                  listOfGenresFromAPI: genreResponse.data.categories.items
+                })
+              });
+            });
+
+
+      }, [genres.selectedGenre, clientID, secret]);
+
+      const genreChanged = val => {
+        setGenres({
+          selectedGenre: val, 
+          listOfGenresFromAPI: genres.listOfGenresFromAPI
+        });
+
+        axios(`https://api.spotify.com/v1/browse/categories/${val}/playlists?limit=10`, {
+            method: 'GET',
+            headers: { 'Authorization' : 'Bearer ' + token}
+          })
+          .then(playlistResponse => {
+            setPlaylist({
+              selectedPlaylist: playlist.selectedPlaylist,
+              listOfPlaylistFromAPI: playlistResponse.data.playlists.items
+            })
+          });
+
+      }
+
+      const playlistChanged = val => {
+        console.log(val);
+        setPlaylist({
+          selectedPlaylist: val,
+          listOfPlaylistFromAPI: playlist.listOfPlaylistFromAPI
+        });
+      }
+
+      const buttonClicked = e => {
+          e.preventDefault();
+      
+          axios(`https://api.spotify.com/v1/playlists/${playlist.selectedPlaylist}/tracks?limit=10`, {
+            method: 'GET',
+            headers: {
+              'Authorization' : 'Bearer ' + token
+            }
+          })
+          .then(tracksResponse => {
+            setTracks({
+              selectedTrack: tracks.selectedTrack,
+              listOfTracksFromAPI: tracksResponse.data.items
+            })
+          });
+      }
+
+      const listboxClicked = val => {
+
+          const currentTracks = [...tracks.listOfTracksFromAPI];
+      
+          const trackInfo = currentTracks.filter(t => t.track.id === val);
+      
+          setTrackDetail(trackInfo[0].track);
+      
+    
+    
+      }
+
+
+    return(
+       <form onSubmit={buttonClicked}>
+          <div className="container">
+          <Dropdown options={genres.listOfGenresFromAPI} selectedValue={genres.selectedValue} changed={genreChanged}/>
+          <Dropdown options={playlist.listOfPlaylistFromAPI} selectedValue={playlist.selectedPlaylist} changed={playlistChanged} />
+          <button type="submit">
+            Submit
+          </button>
+        </div>
+        <div className="row">
+        <Listbox items={tracks.listOfTracksFromAPI} clicked={listboxClicked} />
+            {trackDetail && <Detail {...trackDetail} /> }
+        </div>
+       </form>
+       
+    );
+}
+
+export default TutorialPage;
