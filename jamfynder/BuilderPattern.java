c666cdde644edcfe0c1b5aa14e1febef059323df
Ali Janbeih <alijanbeih7898@gmail.com>
2022-11-07 22:42:57 -0500

    Added Builder design pattern

---
+++ jamfynder/BuilderPattern.java
@@ -0,0 +1,12 @@
+class Playlist {
+    private String userPlaylist;
+    private String song;
+    public void setPlaylist(String userPlaylist) {
+        this.userPlaylist = userPlaylist;
+    }
+   
+    public void setSong(String song) {
+        this.song = song;
+    }
+
+}
