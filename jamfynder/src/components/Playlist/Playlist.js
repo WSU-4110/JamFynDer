abbb40b5bd480a8b32be816d2127da3504c4101c
Ali Janbeih <alijanbeih7898@gmail.com>
2022-11-10 18:47:16 -0500

    Added search bar for playlist generation

--- jamfynder/src/components/Playlist/Playlist.js
+++ jamfynder/src/components/Playlist/Playlist.js
@@ -1,13 +1,54 @@
-import React from "react";
+import React, { Component }  from 'react';
+import { useState } from "react";
 import "./Playlist.css";
+var data = require("./MockData.json");
 
-const Playlist = () => {
+export default function App() {
+  const [value, setValue] = useState("");
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const onSearch = (searchTerm) => {
+    setValue(searchTerm);
+    // our api to fetch the search result
+    console.log("search ", searchTerm);
+  };
+
   return (
-      <div className="playlist">
-          <h1>Want to create your own playlist?</h1>
-          <p>Start here!</p>
-      </div>
-  );
-};
+    <div className="App">
+      <h1>Search for song title</h1>
 
-export default Playlist;
+      <div className="search-container">
+        <div className="search-inner">
+          <input type="text" value={value} onChange={onChange} />
+          <button onClick={() => onSearch(value)}> Search </button>
+        </div>
+        <div className="dropdown">
+          {data
+            .filter((item) => {
+              const searchTerm = value.toLowerCase();
+              const fullName = item.full_name.toLowerCase();
+
+              return (
+                searchTerm &&
+                fullName.startsWith(searchTerm) &&
+                fullName !== searchTerm
+              );
+            })
+            .slice(0, 10)
+            .map((item) => (
+              <div
+                onClick={() => onSearch(item.full_name)}
+                className="dropdown-row"
+                key={item.full_name}
+              >
+                {item.full_name}
+              </div>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+}
