319249d89d33008c2044144a0eaaeb955f10c3fe
Michael Tocco <fj4565@wayne.edu>
2022-11-21 12:25:58 -0500

    added test for attention getter on about page

--- jamfynder/src/components/AboutUs/AboutUs.test.js
+++ jamfynder/src/components/AboutUs/AboutUs.test.js
@@ -8,7 +8,14 @@ describe('About Us', () => {
     const description = screen.getByText("Hello,Welcome to JamFynDer!");
     expect(description).toBeInTheDocument();
   });
-  
+
+  test('Attention getter in description appears on page', () => {
+    render(<AboutUs/>);
+    const description = screen.getByText("Want to discover new music and update your playlists to the latest music trends? Then you have come to the right place!");
+    expect(description).toBeInTheDocument();
+  });
+
+
   
   
 })
