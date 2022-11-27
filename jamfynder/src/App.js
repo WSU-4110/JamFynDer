f40c6e98db83a3ab5ef240181860f908f3222a27
Michael Tocco <fj4565@wayne.edu>
2022-11-27 14:15:34 -0500

    made changes to src and server files, downgraded to react 17

--- jamfynder/src/App.js
+++ jamfynder/src/App.js
@@ -1,61 +1,30 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import WebPlayback from './WebPlayback'
+import Login from './Login'
+import './App.css';
 
-import "./App.css";
-import WelcomePage from "./components/WelcomePage";
-import NavigationBar from "./components/NavigationBar";
-import TutorialPage from "./components/TutorialPage";
-import SettingsPage from "./components/SettingsPage";
-import AboutUs from "./components/AboutUs";
-import Player from "./components/Player";
+function App() {
 
+  const [token, setToken] = useState('');
 
-const CLIENT_ID = "92559e9d1a7f45cd87669f2d2194753f";
-const ENDPOINT = "https://accounts.spotify.com/authorize";
-const REDIRECT_URL = "http://localhost:3000/Player";
-const SPACE_DELIMITER = "%20";
-const SCOPES = ["user-read-currently-playing", 
-                "user-read-playback-state", 
-                "user-modify-playback-state", 
-                "streaming",
-                "playlist-read-private",
-                "playlist-modify-private",
-                "playlist-modify-public"
-                ];
-const SCOPES_URL_PARM = SCOPES.join(SPACE_DELIMITER);
+  useEffect(() => {
 
-const App = () => {
-  
+    async function getToken() {
+      const response = await fetch('/auth/token');
+      const json = await response.json();
+      setToken(json.access_token);
+    }
+
+    getToken();
+
+  }, []);
+
   return (
-    <Router>
-      <div className="App">
-        <NavigationBar />
-        <div className="content">
-          <Switch>
-            <Route path="/welcome">
-              <WelcomePage />
-            </Route>
-
-            <Route path="/Player">
-              <Player />
-            </Route>
-
-            <Route path="/SettingsPage">
-              <SettingsPage />
-            </Route>
-
-            <Route path="/about_us">
-              <AboutUs />
-            </Route>
-
-            <Route path="/tutorial">
-              <TutorialPage />
-            </Route>
-          </Switch>
-        </div>
-      </div>
-    </Router>
+    <>
+        { (token === '') ? <Login/> : <WebPlayback token={token} /> }
+    </>
   );
-};
+}
+
 
 export default App;
