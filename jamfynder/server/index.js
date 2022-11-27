9acee131dc99de585014fe210b83517c29222476
Michael Tocco <fj4565@wayne.edu>
2022-11-26 22:50:15 -0500

    added code to index.js for Request User Authorization

--- jamfynder/server/index.js
+++ jamfynder/server/index.js
@@ -8,9 +8,42 @@ dotenv.config()
 var spotify_client_id = process.env.SPOTIFY_CLIENT_ID
 var spotify_client_secret = process.env.SPOTIFY_CLIENT_SECRET
 
+
+var generateRandomString = function (length) {
+  var text = '';
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
 var app = express();
 
 app.get('/auth/login', (req, res) => {
+  var scope = "streaming \
+               user-read-email \
+               user-read-private \
+               user-read-currently-playing \
+               user-read-playback-state \
+               user-modify-playback-state \
+               playlist-read-private \
+               playlist-read-private \
+               playlist-modify-private \
+               playlist-modify-public"
+
+  var state = generateRandomString(16);
+
+  var auth_query_parameters = new URLSearchParams({
+    response_type: "code",
+    client_id: spotify_client_id,
+    scope: scope,
+    redirect_uri: "http://localhost:3000/auth/callback",
+    state: state
+  })
+
+  res.redirect('https://accounts.spotify.com/authorize/?' + auth_query_parameters.toString());
 });
 
 app.get('/auth/callback', (req, res) => {
