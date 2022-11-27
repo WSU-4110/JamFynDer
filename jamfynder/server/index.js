b79956c6bf6b8ecf07f0852bacecfeb8ad808544
Michael Tocco <fj4565@wayne.edu>
2022-11-26 22:55:33 -0500

    added code in index.js for Request Access Token

--- jamfynder/server/index.js
+++ jamfynder/server/index.js
@@ -47,6 +47,28 @@ app.get('/auth/login', (req, res) => {
 });
 
 app.get('/auth/callback', (req, res) => {
+  var code = req.query.code;
+
+  var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    form: {
+      code: code,
+      redirect_uri: "http://localhost:3000/auth/callback",
+      grant_type: 'authorization_code'
+    },
+    headers: {
+      'Authorization': 'Basic ' + (Buffer.from(spotify_client_id + ':' + spotify_client_secret).toString('base64')),
+      'Content-Type' : 'application/x-www-form-urlencoded'
+    },
+    json: true
+  };
+
+  request.post(authOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var access_token = body.access_token;
+      res.redirect('/')
+    }
+  });
 });
 
 app.listen(port, () => {
