51023f4109c0da70c7c07937ccb4f6ceb856984f
srazzaq01 <shehrozerazzaq@gmail.com>
2022-11-28 12:56:29 -0500

    adding information to playback component

--- jamfynder/src/components/TEST/Playback.js
+++ jamfynder/src/components/TEST/Playback.js
@@ -1,7 +1,7 @@
 import React from "react";
 import SpotifyPlayer from "react-spotify-web-playback"
 
-export default function Playback({ accesToken }) {
+export default function Playback({ accesToken, trackUri }) {
     return (<SpotifyPlayer 
     token={accesToken}
     uris={trackUri ? [trackUri] : []}
