fbee66b0cc9bb3e12f7502c4fe74ce1dd7eee15b
Henry Winczner <go9126@wayne.edu>
2022-12-01 19:54:12 -0500

    updating branch to current ver

--- jamfynder/src/components/WelcomePage/WelcomePage.js
+++ jamfynder/src/components/WelcomePage/WelcomePage.js
@@ -1,13 +1,20 @@
 import React from 'react'
 // import NavigationBar from '../NavigationBar'
+import "./WelcomePage.css";
 
 const WelcomePage = () => {
+ 
   return (
-    <div className="welcome">
+    <div class="blob-wrap x">
+  <div class="blob y"> <div class = "blob-wrap blob-2 x2"> 
+  </div></div></div>
+
+    /*<div className="welcome">
       <h1>Welcome to JamFynDer</h1>
       <p>Where good music finds you</p>
-    </div>
+    </div>*/
   );
 }
 
 export default WelcomePage;
+
