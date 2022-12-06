e433e87c28ec0b84dd725fbb606afbfb5aae246f
Chris Wilson <awil17@outlook.com>
2022-12-06 04:57:22 -0500

    Added more functionality to profile page

--- jamfynder/src/components/Player/Player.js
+++ jamfynder/src/components/Player/Player.js
@@ -2,12 +2,20 @@ import { useEffect, useState } from "react";
 import React from "react";
 import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { accessToken, logout, getCurrentUserProfile } from "../Spotify/spotify";
+import {
+  accessToken,
+  logout,
+  getCurrentUserProfile,
+  getCurrentUserPlaylists,
+  getTopArtists,
+} from "../Spotify/spotify";
 import "./Player.css";
 
 const Player = () => {
   const [token, setToken] = useState(null);
   const [profile, setProfile] = useState(null);
+  const [playlists, setPlaylists] = useState(null);
+  const [topArtists, setTopArtists] = useState(null);
 
   useEffect(() => {
     setToken(accessToken);
@@ -16,6 +24,12 @@ const Player = () => {
       try {
         const { data } = await getCurrentUserProfile();
         setProfile(data);
+
+        const userPlaylists = await getCurrentUserPlaylists();
+        setPlaylists(userPlaylists.data);
+
+        const userTopArtist = await getTopArtists();
+        setTopArtists(userTopArtist.data);
       } catch (e) {
         console.error(e);
       }
@@ -23,16 +37,35 @@ const Player = () => {
 
     fetchData();
   }, []);
+
   return (
     <body className="main">
       <div>
         {profile && (
-          <div>
-            <h1>{profile.display_name}</h1>
-            <p>{profile.followers.total} Followers</p>
-            {profile.images.length && profile.images[0].url && (
-              <img src={profile.images[0].url} alt="Avatar" />
-            )}
+          <div className="Content">
+            <div className="Banner">
+              {profile.images.length && profile.images[0].url && (
+                <img
+                  className="profileImg"
+                  src={profile.images[0].url}
+                  alt="Avatar"
+                />
+              )}
+              <h1>{profile.display_name}</h1>
+              <p className="sub-heading">{profile.followers.total} Followers</p>
+              <p className="header__meta">
+                {playlists && (
+                  <span className="sub-heading">
+                    {playlists.total} Playlist{playlists.total !== 1 ? "s" : ""}
+                  </span>
+                )}
+                <span className="sub-heading">
+                  {profile.followers.total !== 1 ? "s" : ""}
+                </span>
+              </p>
+            </div>
+
+            <div className="Content"></div>
           </div>
         )}
       </div>
--- jamfynder/src/components/Spotify/spotify.js
+++ jamfynder/src/components/Spotify/spotify.js
@@ -143,3 +143,11 @@ axios.defaults.headers["Content-Type"] = "application/json";
  * @returns {Promise}
  */
 export const getCurrentUserProfile = () => axios.get("/me");
+
+export const getCurrentUserPlaylists = (limit = 20) => {
+  return axios.get(`/me/playlists?limit=${limit}`);
+};
+
+export const getTopArtists = (time_range = "short_term") => {
+  return axios.get(`/me/top/artists?time_range=${time_range}`);
+};
